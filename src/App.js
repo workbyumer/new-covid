fef71b8b051714e2d4cb6f54096eef18f3ca7d17
Umer <workbyumer@gmail.com>
2023-07-24 18:37:03 +0500

    Table shows when clicked on Marker

--- src/App.js
+++ src/App.js
@@ -11,21 +11,65 @@ import Loaders from "./componets/loader";
 const App = () => {
   const [country, setcountry] = useState("Pakistan");
   const [countryData, setcountryData] = useState({});
+  const [countryHistory, setcountryHistory] = useState({});
   const [isLoaded, setisLoaded] = useState(false);
   useEffect(() => {
+    setisLoaded(true);
     const fetchcountryData = () => {
       axios
         .get(`https://disease.sh/v3/covid-19/countries/${country}`)
         .then((res) => {
-          setisLoaded(true);
           const data = res.data.countryInfo;
           setcountryData(data);
-          setisLoaded(false);
+        })
+        .catch((error) => console.log(error));
+    };
+    const fetchcountryHistory = () => {
+      axios
+        .get(
+          `https://disease.sh/v3/covid-19/historical/${country}?lastdays=all`
+        )
+        .then((res) => {
+          const data = res.data.timeline;
+          const monthlyData = groupDataByMonth(data);
+          setcountryHistory(monthlyData);
         })
         .catch((error) => console.log(error));
     };
     fetchcountryData();
+    fetchcountryHistory();
+    setisLoaded(false);
   }, [country]);
+  const groupDataByMonth = (data) => {
+    const dates = Object.keys(data.cases);
+    const monthlyData = {};
+
+    dates.forEach((date) => {
+      const [month, day, year] = date.split("/");
+      const key = `${year}-${month.padStart(2, "0")}`;
+      if (!monthlyData[key]) {
+        monthlyData[key] = {
+          cases: 0,
+          deaths: 0,
+          recovered: 0,
+        };
+      }
+
+      monthlyData[key].cases += data.cases[date];
+      monthlyData[key].deaths += data.deaths[date];
+      monthlyData[key].recovered += data.recovered[date];
+    });
+
+    const monthlyDataArray = Object.entries(monthlyData).map(
+      ([month, value]) => ({
+        month,
+        ...value,
+      })
+    );
+
+    return monthlyDataArray;
+  };
+
   return isLoaded === false ? (
     <Box>
       <Box>
@@ -37,7 +81,16 @@ const App = () => {
       <Routes>
         <Route path="/table" element={<TableData />} />
         <Route path="/chart" element={<ChartData />} />
-        <Route path="/map" element={<MapData locationData={countryData} />} />
+        <Route
+          path="/map"
+          element={
+            <MapData
+              locationData={countryData}
+              tableData={countryHistory}
+              name={country}
+            />
+          }
+        />
       </Routes>
     </Box>
   ) : (
